487661ea658deb27fcbcde2a2ba1dcb600116396
denis554 <denisignatenko554@gmail.com>
2014-09-22 17:37:06 +1000

    Keep it DRY

--- setup.py
+++ setup.py
@@ -3,14 +3,15 @@ from setuptools import setup
 
 VERSION  = '0.2.3'
 REQUIRES = ['simplejson', 'httplib2']
+URL      = 'https://github.com/realestate-com-au/cartodb-dashboard-python'
 
 setup(name             = 'cartodb_dashboard',
       author           = 'Simon Hope',
       author_email     = 'shope@geoplex.com.au',
       description      = 'Provides access to the cartodb dashboard',
       version          = VERSION,
-      url              = 'https://github.com/realestate-com-au/cartodb-dashboard-python',
-      download_url     = 'https://github.com/realestate-com-au/cartodb-dashboard-python/tarball/' + VERSION,
+      url              = URL,
+      download_url     = URL + '/tarball/' + VERSION,
       install_requires = REQUIRES,
       packages         = ['cartodb_dashboard'],
       requires         = REQUIRES,
