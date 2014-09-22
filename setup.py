e797a242a3eb63ecb5cf334008f1a912e172e46d
denis554 <denisignatenko554@gmail.com>
2014-09-22 16:11:58 +1000

    Bump version :pear: leon

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ from setuptools import setup
 
 REQUIRES = ['simplejson', 'httplib2', 'mako']
 
-setup(name='cartodb_dashboard', 
-      author = 'Simon Hope', 
+setup(name='cartodb_dashboard',
+      author = 'Simon Hope',
       author_email = 'shope@geoplex.com.au',
       description = 'Provides access to the cartodb dashboard',
-      version='0.2.1',
+      version='0.2.3',
       url='https://github.com/realestate-com-au/cartodb-dashboard-python',
       download_url = 'https://github.com/realestate-com-au/cartodb-dashboard-python/tarball/0.2.1',
       install_requires=REQUIRES,
