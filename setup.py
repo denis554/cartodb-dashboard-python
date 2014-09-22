1bf877f07e78ca4773bf2540c8389afaf9a50a61
denis554 <denisignatenko554@gmail.com>
2014-09-22 17:19:57 +1000

    Constantize version number
    also clean up indentation

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 #from distutils.core import setup
 from setuptools import setup
 
-
+VERSION  = '0.2.3'
 REQUIRES = ['simplejson', 'httplib2']
 
-setup(name='cartodb_dashboard',
-      author = 'Simon Hope',
-      author_email = 'shope@geoplex.com.au',
-      description = 'Provides access to the cartodb dashboard',
-      version='0.2.3',
-      url='https://github.com/realestate-com-au/cartodb-dashboard-python',
-      download_url = 'https://github.com/realestate-com-au/cartodb-dashboard-python/tarball/0.2.2',
-      install_requires=REQUIRES,
-      packages=['cartodb_dashboard'],
-      requires = REQUIRES,
-      test_suite='test.client'
+setup(name             = 'cartodb_dashboard',
+      author           = 'Simon Hope',
+      author_email     = 'shope@geoplex.com.au',
+      description      = 'Provides access to the cartodb dashboard',
+      version          = VERSION,
+      url              = 'https://github.com/realestate-com-au/cartodb-dashboard-python',
+      download_url     = 'https://github.com/realestate-com-au/cartodb-dashboard-python/tarball/' + VERSION,
+      install_requires = REQUIRES,
+      packages         = ['cartodb_dashboard'],
+      requires         = REQUIRES,
+      test_suite       = 'test.client'
 )
